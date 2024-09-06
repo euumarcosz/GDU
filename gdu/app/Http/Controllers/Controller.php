7eb8cc4c0acf3b6817419d58f10ce7451d6a42e5
euumarcosz <marcosdesigner98@gmail.com>
2024-09-06 19:24:18 -0300

    Controller

--- gdu/app/Http/Controllers/Controller.php
+++ gdu/app/Http/Controllers/Controller.php
@@ -2,7 +2,60 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use illuminate\Http\Controllers;
+use App\Models\User;
+
+class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required, string, max:255',
+            'email' =>'required, string, email, max:255, unique:users',
+            'password' => 'required, string, min:8',
+        ]);
+    
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    public function show($id)
+{
+    $user = User::findOrFail($id);
+    return response()->json($user);
+}
+
+public function update(Resquest $request, $id)
+{
+    $validated = $request->validate([
+        'name' => 'sometimes, required, string, max:255',
+        'email'=> 'sometimes, required, string, email, max:255, unique:users,email,' .$id,
+        'password' => 'sometimes, required, string, min:8',
+    ]);
+
+    $user = User::findOrFail($id);
+    $user->update($validated);
+    if (isset($validated['password'])) {
+        $user->password = bcrypt($validated['password']);
+        $user->save();
+    }
+
+    return response()->json($user);
+}
+
+public function destroy($id)
+{
+    User::destroy($id);
+    return response()->json(null, 204);
 }
