31c8e8afc25048319551b0fa037c4fa89ea9a334
euumarcosz <marcosdesigner98@gmail.com>
2024-09-08 12:34:41 -0300

    ControllerUser

--- gdu/app/Http/Controllers/UserController.php
+++ gdu/app/Http/Controllers/UserController.php
@@ -5,43 +5,46 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\Response; // Certifique-se de que está importando corretamente
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return Response::json($users);
+        return view('users.index', compact('users'));
     }
 
     public function store(Request $request)
     {
         $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
-        return Response::json($user);
+
+        return response()->json($user, 201);
     }
 
     public function show($id)
     {
         $user = User::findOrFail($id);
-        return Response::json($user);
+        return response()->json($user);
     }
 
     public function update(Request $request, $id)
     {
         $user = User::findOrFail($id);
-        $user->update($request->only(['name', 'email', 'password']));
-        return Response::json($user);
+        $user->update($request->all());
+
+        return response()->json($user);
     }
 
     public function destroy($id)
     {
         $user = User::findOrFail($id);
         $user->delete();
-        return Response::json(['message' => 'User deleted']);
+
+        return response()->json(null, 204);
     }
 }
