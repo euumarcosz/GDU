df9478edc325053201d5addecc6e8840d918a1f5
euumarcosz <marcosdesigner98@gmail.com>
2024-09-08 10:41:57 -0300

    build: :rocket:

--- gdu/app/Http/Controllers/UserController.php
+++ gdu/app/Http/Controllers/UserController.php
@@ -4,59 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return response()->json($users);
+        return Response::json($users);
     }
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
-
         $user = User::create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => bcrypt($validated['password']),
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
         ]);
-
-        return response()->json($user, 201);
+        return Response::json($user);
     }
 
     public function show($id)
     {
         $user = User::findOrFail($id);
-        return response()->json($user);
+        return Response::json($user);
     }
 
     public function update(Request $request, $id)
     {
-        $validated = $request->validate([
-            'name' => 'sometimes|required|string|max:255',
-            'email' => 'sometimes|required|string|email|max:255|unique:users,email,' . $id,
-            'password' => 'sometimes|required|string|min:8',
-        ]);
-
         $user = User::findOrFail($id);
-        $user->update($validated);
-        if (isset($validated['password'])) {
-            $user->password = bcrypt($validated['password']);
-            $user->save();
-        }
-
-        return response()->json($user);
+        $user->update($request->only(['name', 'email', 'password']));
+        return Response::json($user);
     }
 
     public function destroy($id)
     {
-        User::destroy($id);
-        return response()->json(null, 204);
+        $user = User::findOrFail($id);
+        $user->delete();
+        return Response::json(['message' => 'User deleted']);
     }
 }
